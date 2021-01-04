e19b6a8632c30fc2be126bd2a1effd7041d16cef
Jothamsl <jothamsl1@gmail.com>
2021-01-04 21:07:16 +0100

    Modified Namespaces.cpp & Cout.cpp

--- 1.Cout.cpp
+++ 1.Cout.cpp
@@ -6,11 +6,11 @@
 int main(void)
 {
     std::cout << "Hello to the cpp world of programming!";
-    //  ^         ^                 ^                    ^
-    //Target    write        Message (object)        Terminator
+    //  ^      ^                  ^                      ^
+    //Target  write        Message (object)          Terminator
     //            on
     std::cout << "To show the main structure." << std::endl;
-    //                                                    ^
-    //                                               Same as \n
+    //                                                  ^
+    //                                             Same as \n
     return 0;
 }
--- 2.Namespace.cpp
+++ 2.Namespace.cpp
@@ -5,6 +5,6 @@ int main(void)
 {
     cout << "Name spaces (using namespace std)" << endl; 
     cout << "allow us to use an object" << endl;
-    cout << "without having to use it's reference library";
+    cout << "without having to diretly access it's reference library";
     return 0;
 }
