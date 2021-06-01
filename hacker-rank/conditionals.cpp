de27d8ac4634e61d4926041af418bc18bd2fc53f
Jotham <jotham@pop-os.localdomain>
2021-06-01 21:41:39 +0100

    Worked on Hacker-rank problems

---
+++ hacker-rank/conditionals.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+int main()
+{ 
+    int n;
+    cin >> n; 
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    return 0;
+}
---
+++ hacker-rank/input-output.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+
+int main(void)
+{ 
+    int a, b, c;
+    std::cin >> a >> b >> c;
+    std::cout << a + b + c << "\n";
+    return 0;
+}
